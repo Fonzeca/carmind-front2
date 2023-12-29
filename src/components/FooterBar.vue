9e4ddad53f6dfef4b58049af76e2f6ee5564d5d4
Alexis <alexisfonzos_@hotmail.com>
2023-12-29 12:22:20 -0300

    Update styles in FooterBar.vue and IconCar.vue

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -49,7 +49,7 @@ export default defineComponent({
   <div class="flex h-[64px] flex-row items-center justify-around w-full text-white bg-bg_menu">
     <div class="flex flex-col items-center min-w-[60px] cursor-pointer" v-for="item,index in options" :key="item.route" @click="click(index)">
       <component :is="item.icon" class="w-[35px] h-[35px]" :is-selected="index === selected"></component>
-      <span class="text-xs" :class="{'text-accent-500' : index === selected}">{{item.label}}</span>
+      <span class="text-xs" :class="{'text-accent' : index === selected}">{{item.label}}</span>
     </div>
   </div>
 </template>

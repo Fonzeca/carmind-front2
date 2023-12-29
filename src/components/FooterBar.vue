c6167fb94e4af6e06d2d0d8d6ffb38758b1b4539
Alexis <alexisfonzos_@hotmail.com>
2023-12-29 12:19:46 -0300

    Update styles in FooterBar, NormalButton, and LoginView

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -46,7 +46,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="flex h-[64px] flex-row items-center justify-around w-full text-white bg-gris-500">
+  <div class="flex h-[64px] flex-row items-center justify-around w-full text-white bg-bg_menu">
     <div class="flex flex-col items-center min-w-[60px] cursor-pointer" v-for="item,index in options" :key="item.route" @click="click(index)">
       <component :is="item.icon" class="w-[35px] h-[35px]" :is-selected="index === selected"></component>
       <span class="text-xs" :class="{'text-accent-500' : index === selected}">{{item.label}}</span>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -60,7 +60,7 @@ export default defineComponent({
             <NormalInput v-model:value="password" class="mb-9" :hide-value="!showPass">
                 <div class="flex flex-col justify-center h-full w-[30px] cursor-pointer select-none"
                     @click="showPass = !showPass">
-                    <IconEye v-if="!showPass"></IconEye>
+                    <IconEye v-if="showPass"></IconEye>
                     <IconEyeSlash v-else></IconEyeSlash>
 
                 </div>

f0461c7ab87702dc8b364d35d8582658c9de2395
Daiko <daikoponce@gmail.com>
2024-09-26 15:07:57 -0300

    Added pencil svg

--- src/views/mains/ProfileView.vue
+++ src/views/mains/ProfileView.vue
@@ -2,16 +2,22 @@
 import { authStore } from '@/stores/auth';
 import { mapStores } from 'pinia';
 import { defineComponent } from 'vue';
+import Pencil from '@/components/icons/Pencil.vue';
 
 
 
 export default defineComponent({
   data() {
     return {
-      editField: false,
+      editName: false,
+      editLastname: false,
+      editDocument: false,
       name:"Alexis",
       lastname:"Fonzo",
       document:"44444444",
+      editedName: "",
+      editedLastname:"",
+      editedDocument:"",
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8,
       options: {}
@@ -23,12 +29,23 @@ export default defineComponent({
       this.$router.push('/');
     },
     //edit profile data method
-    async edit() {
+    async editNameMethod() {
+      this.editName=false;
+      this.name = this.editedName;
+    },
+    async editLastnameMethod(){
+      this.editLastname=false;
+      this.lastname= this.editedLastname;
+    },
+    async editDocumentMethod(){
+      this.editDocument=false;
+      this.document= this.editedDocument;
     }
   },
   computed: {
     ...mapStores(authStore)
-  }
+  },
+  components: {Pencil}
 })
 </script>
 
@@ -43,7 +60,7 @@ export default defineComponent({
       <div class="flex text-xl text-gray-500">
         Datos de la cuenta
       </div>
-      <div class="max-w-4xl  bg-grey w-full rounded-lg shadow-lg">
+      <div class="flex flex-col max-w-4xl  bg-grey w-full rounded-lg shadow-lg">
         <div class="grid2columns">
           <p class="text-gray-600">
             Empresa
@@ -76,41 +93,69 @@ export default defineComponent({
 
 
           <!-- edit mode enabled -->
-           <div v-if="editField" >
-             <input v-model="name">
-             <div class="flex items-center justify-center space-x-2">
-               <button type="button" @click="editField = false" class="btn rounded bg-gray-200 font-medium hover:bg-gray-300">X</button>
-               <button type="button" @click="editField = false" class="btn rounded bg-blue-500 font-medium text-white hover:bg-blue-600"></button>
+           <div v-if="editName" >
+             <input v-model="editedName">
+             <div class="flex flex-row items-center justify-center space-x-2">
+               <button type="button" @click="editName=false" class="btn rounded bg-red-500 font-medium text-white hover:bg-red-600">X</button>
+               <button type="button" @click="editNameMethod()" class="btn rounded bg-green-500 font-medium text-white hover:bg-green-600">✓</button>
               </div>
             </div>
 
 
-          <!-- disabled when edit mode is disabled too-->
-          <input v-else disabled v-model="name">
+          <!-- edit mode disabled -->
+          <div v-else class="flex flex-row">
+            <input  disabled v-model="name">
+            <div class="cursor-pointer" @click="editName = true, editedName = name"><Pencil> </Pencil></div>
+
+          </div>
             
         </div>
         <div class="grid2columns">
           <p class="text-gray-600">
             Apellido
           </p>
-          <p>
-            Fonzo
-          </p>
+            <!-- edit mode enabled -->
+           <div class="flex items-center justify-between" v-if="editLastname">
+             <input v-model="editedLastname">
+             <div class="flex flex-row items-center justify-center space-x-2">
+               <button type="button" @click="editLastname = false" class="btn rounded bg-red-500 font-medium text-white hover:bg-red-600">X</button>
+               <button type="button" @click="editLastnameMethod()" class="btn rounded bg-green-500 font-medium text-white hover:bg-green-600">✓</button>
+              </div>
+            </div>
+
+
+          <!-- edit mode disabled -->
+          <div v-else class="flex flex-row">
+            <input  disabled v-model="lastname">
+            <div class="cursor-pointer" @click="editLastname = true, editedLastname= lastname"><Pencil> </Pencil></div>
+
+          </div>
         </div>
         <div class="grid2columns">
-          <p class="text-gray-600">
+          <div class="text-gray-600">
             Documento
-          </p>
-          <p>
-            44444444
-          </p>
+          </div>
+            <!-- edit mode enabled -->
+           <div class="flex flex-row items-center justify-between" v-if="editDocument">
+             <input class="flex w-[120px]" v-model="editedDocument">
+             <div class="flex flex-row items-center justify-center">
+               <button type="button" @click="editDocument = false" class="btn rounded font-medium text-red-500">X</button>
+               <button type="button" @click="editDocumentMethod()" class="btn rounded font-medium text-green-500">✓</button>
+              </div>
+            </div>
+
+
+          <!-- edit mode disabled -->
+          <div v-else class="flex flex-row">
+            <input  disabled v-model="document">
+            <div class="cursor-pointer" @click="editDocument = true, editedDocument= document"><Pencil> </Pencil></div>
+
+          </div>
         </div>
       </div>
 
 
-      <div class="flex grid p-6 place-items-center">
-        <div class="px-6 my-3 whitespace-nowrap box-content text-cyan-500 hover:text-cyan-700 cursor-pointer" @click="editField = true">Edit data</div>
-        <span></span>
+      <div class="flex p-6 place-items-center">
         <div class="px-6 py-3 w-min text-white bg-red-600 rounded cursor-pointer hover:bg-red-400" @click="logout">Logout</div>
       </div>
     </div>
